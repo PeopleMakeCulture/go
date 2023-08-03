4b644555a70a0597645eae417be56f248d0304b4
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-08-03 13:21:04 -0400

    add slices part 2

--- hello/arrays.go
+++ hello/arrays.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func static_array() {
 	// Static arrays are of fixed size
@@ -10,8 +13,8 @@ func static_array() {
 	/*FOR LOOPS*/
 	// NOTE: go doesn't use parens around looping condition
 	// (Option 1)
-	fmt.Println("For loop the javascripty way")
-	for i:= 0; i < len(DeploymentOptions); i++ {
+	fmt.Println("Every other value")
+	for i:= 0; i < len(DeploymentOptions); i+=2 {
 		option := DeploymentOptions[i]
 		fmt.Println(i, option)
 	}
@@ -38,13 +41,38 @@ func slice(){
 	classics := languages[:3] // [0:3]
 	new := languages[7:] // [7:9]
 
-	fmt.Printf("classics: %v\n", classics)
-	fmt.Printf("new: %v\n", new)
 
 	modern := make([]string, 4) // declare
 	modern = languages[3:7] // assign
 
+	allLangs := languages[:] // copy array to create a slice
+	
+	fmt.Printf("classics: %v\n", classics)
+	fmt.Printf("new: %v\n", new)
 	fmt.Printf("modern languages: %v\n", modern)
+	fmt.Printf("type of languages: %v\n", reflect.TypeOf(languages).Kind())
+	fmt.Printf("type of allLangs: %v\n", reflect.TypeOf(allLangs).Kind()) // slice
+
+	// we can create a slice by not explicit stating size of array
+	frameworks := []string{
+		"React", "Vue", "Angular", "Svelte",
+		"Laravel", "Django", "Flask", "Fiber",
+	}
+
+		// go is not indent sensitive
+		fmt.Printf("type of frameworks: %v\n", reflect.TypeOf(frameworks).Kind())
+
+	jsFrameworks := frameworks[:4:4] //length 4 capacity 4
+    
+    fmt.Printf("js frameworks: %v\n", jsFrameworks)
+    fmt.Printf("all frameworks: %v\n", frameworks)
+
+    // NOTE: append() creates a new slice of same type as original 
+    // we need to re-assign the new array to the original variable
+    frameworks = append(frameworks, "Meteor")
+
+    fmt.Printf("all frameworks updated: %v\n", frameworks)
+
 }
 
 func main(){
