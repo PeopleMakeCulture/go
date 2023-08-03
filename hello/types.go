f0250ddb777e8bcf30bee0f7108d9dbe2bc871b3
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-08-03 13:43:19 -0400

    add user specified type

--- hello/types.go
+++ hello/types.go
@@ -11,7 +11,7 @@ func main(){
 	var c complex128 = 1 + 4i
 	var d uint16 = 14 // 16-bit unsigned int
 
-	// Go also handles implicit typing!
+	// Default types-- Go also handles implicit typing!
 	n := 42 // rune
 	pi := 3.14 // 32-bit float
 	x, y := true, false // bool - note, no caps
@@ -22,4 +22,18 @@ func main(){
 
 	fmt.Printf("user-specified types: %T %T %T %T\n", a, b, c, d)
 	fmt.Printf("default types:%T %T %T %T %T\n", n, pi, x, y, z)
+
+	// You can also create your own types (eg ADTs/classes?)
+	type Person struct {
+		name 	string
+		age 	int
+	}
+
+	first_person := Person{
+		name: "Eve",
+		age: 100,
+	}  
+
+	fmt.Printf("Hi my name is %s and I am %d years old.", first_person.name, first_person.age)
+
 }
