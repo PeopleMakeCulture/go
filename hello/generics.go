d3bfe7595f82bb24fd881b59e65fd0dd6f6c5f81
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-08-04 11:04:42 -0400

    update generics comments

--- hello/generics.go
+++ hello/generics.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
-func get_first_element[T comparable](arr []T) T {
+// [T comparable] says the type T should be comparable
+// (arr []T) says this function takes one argument: a slice of any type
+func get_first_element[T comparable](sl []T) T {
 
-	return arr[0]
+	return sl[0]
 }
 
 func main() {
@@ -12,15 +14,15 @@ func main() {
 	// T = int32
     intSlice := []int32{5, 2, 9, 1, 7}
     minInt := get_first_element(intSlice)
-    fmt.Println("first_element:", minInt)
+    fmt.Println("first_int_element:", minInt)
 
     // T = float64
     floatSlice := []float64{3.14, 1.618, 2.718}
     minFloat := get_first_element(floatSlice)
-    fmt.Println("first_element:", minFloat)
+    fmt.Println("first_float_element:", minFloat)
 
     // T = string
     stringSlice := []string{"apple", "banana", "orange"}
     minString := get_first_element(stringSlice)
-    fmt.Println("first_element:", minString)
+    fmt.Println("first_string_element:", minString)
 }
