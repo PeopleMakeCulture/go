a88d37e00430e774e0a3847f53a63dba4e777a38
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-08-04 10:47:11 -0400

    update maps

--- hello/maps.go
+++ hello/maps.go
@@ -8,20 +8,22 @@ func main(){
 		"Python": 1991, "Javascript": 1996, "Go": 2012,
 	}
 
+	// iterating over all key-value pairs
 	for k, v := range release_dates {
 		fmt.Printf("%s was first released in %d\n", k, v)
 	}
+
 
 	//accessing a non-existent key in a map will return the zero value of the value type
 	fmt.Printf("Golang was first released in %d\n", release_dates["Golang"]) // 0
 
 	// map[key] returns two values, the value, and a boolean for whether that key exists or not
-	_ , exists := release_dates["Go"]
+	_ ,go_ exists := release_dates["Go"]
+	_ , golang_exists = release_dates["Golang"]
 
-	fmt.Printf("It is %t that %s exists\n", exists, "Go")
+	fmt.Printf("It is %t that %s exists\n", go_exists, "Go")
+	fmt.Printf("It is %t that %s exists\n", golang_exists, "Golang")
 
-	_ , exists = release_dates["Golang"]
-
-	fmt.Printf("It is %t that %s exists\n", exists, "Golang")
+	// TODO: ok syntax
 
 }
