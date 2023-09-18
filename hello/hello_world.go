bc748ac672226e516b61bb456316bb39277ffbfb
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-09-18 15:50:20 -0400

    do go.dev/doc/tutorial/call-module-code

--- hello/hello_world.go
+++ hello/hello_world.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"greetings"
+)
 
 func main() {
 	fmt.Println("Hello World") // must use double quotes, single quotes are for one char rune literals
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
